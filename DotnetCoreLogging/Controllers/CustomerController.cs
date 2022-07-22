3373baa449404a7dd7f821cc7483221ba6827c72
Shaban Gomaa <shaban_gomaa@hotmail.com>
2022-07-22 22:46:32 +0200

    first commit

---
+++ DotnetCoreLogging/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetCoreLogging.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+
+        private readonly ILogger<CustomerController> _logger;
+        public CustomerController(ILogger<CustomerController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Get()
+        {
+            _logger.LogInformation("Get customer at "+ DateTime.UtcNow.ToLongTimeString());
+            return "Customer 1";
+        }
+    }
+}
